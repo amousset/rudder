821c183e4ee3243cd265ffb310b50b30ab2f96c9
Alexis Mousset <alexis.mousset@rudder.io>
2019-03-22 16:06:30 +0100

    Remove cli verbosity option

--- relay/sources/relayd/src/cli.rs
+++ relay/sources/relayd/src/cli.rs
@@ -1,12 +1,10 @@
 use crate::configuration::DEFAULT_CONFIGURATION_FILE;
 use clap::{crate_version, App, Arg};
-use slog::Level;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct CliConfiguration {
     pub configuration_file: PathBuf,
-    pub verbosity: Level,
 }
 
 pub fn parse() -> CliConfiguration {
@@ -23,16 +21,6 @@ pub fn parse() -> CliConfiguration {
                 .help("Sets a custom config file")
                 .takes_value(true),
         )
-        .arg(
-            Arg::with_name("verbosity")
-                .short("v")
-                .long("verbosity")
-                .default_value("info")
-                .possible_values(&["error", "warn", "info", "debug", "trace"])
-                .value_name("VERBOSITY")
-                .help("Sets the level of verbosity")
-                .takes_value(true),
-        )
         .get_matches();
 
     CliConfiguration {
@@ -40,10 +28,5 @@ pub fn parse() -> CliConfiguration {
             .value_of("config")
             .expect("No configuration file specified")
             .into(),
-        verbosity: matches
-            .value_of("verbosity")
-            .expect("Missing verbosity level")
-            .parse::<Level>()
-            .expect("Unknown verbosity level"),
     }
 }
